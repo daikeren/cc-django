e524190845523448dbc9800af916d68b84f4146f
Andy Dai <daikeren@gmail.com>
2017-12-15 17:00:02 +0800

    Fix flake8 error

--- {{cookiecutter.project_name}}/src/{{cookiecutter.project_slug}}/settings/base.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.project_slug}}/settings/base.py
@@ -161,7 +161,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = str(ROOT_DIR.path('assets'))
 STATICFILES_DIRS = [
-    str((ROOT_DIR-1).path('static')),
+    str((ROOT_DIR - 1).path('static')),
 ]
 
 # Media files
